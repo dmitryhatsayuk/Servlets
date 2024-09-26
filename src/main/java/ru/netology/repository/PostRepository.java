5b367c50d8fe53984de55292ac907e0f739365cc
dmitryhatsayuk <155708514+dmitryhatsayuk@users.noreply.github.com>
2024-09-26 18:37:48 +0300

    chenges

---
+++ src/main/java/ru/netology/repository/PostRepository.java
@@ -0,0 +1,47 @@
+package ru.netology.repository;
+
+import ru.netology.model.Post;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicLong;
+
+// Stub
+public class PostRepository {
+    public ConcurrentHashMap<Long, Post> posts = new ConcurrentHashMap<>();
+    public AtomicLong lastPostId = new AtomicLong(0);
+
+    public List<Post> all() {
+        return posts.values().stream().toList();
+    }
+
+    public Optional<Post> getById(long id) {
+        return posts.values().stream().filter(post -> post.getId() == id).findFirst();
+    }
+
+    public Post save(Post post) {
+        if (post.getId() != 0L) {
+            posts.put(post.getId(), post);
+        } else {
+            posts.put(findNewId(posts), post);
+        }
+        return post;
+    }
+
+    public void removeById(long id) {
+        posts.remove(id);
+    }
+
+    public Long findNewId(ConcurrentHashMap<Long, Post> posts) {
+        long value = 0;
+        while (posts.containsKey(lastPostId.get())) {
+            value = lastPostId.getAndIncrement();
+        }
+
+        return value;
+
+    }
+
+
+}
---
+++ src/main/java/ru/netology/servlet/MainServlet.java
@@ -0,0 +1,67 @@
+package ru.netology.servlet;
+
+import ru.netology.controller.PostController;
+import ru.netology.repository.PostRepository;
+import ru.netology.service.PostService;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class MainServlet extends HttpServlet {
+    private static final String API_POSTS = "/api/posts";
+    private static final String API_POST_ID = "/api/posts/\\d+";
+    private PostController controller;
+
+    @Override
+    public void init() {
+        final var repository = new PostRepository();
+        final var service = new PostService(repository);
+        controller = new PostController(service);
+    }
+
+    @Override
+    protected void service(HttpServletRequest req, HttpServletResponse resp) {
+        // если деплоились в root context, то достаточно этого
+        try {
+            final var path = req.getRequestURI();
+            final var method = req.getMethod();
+            // primitive routing
+            if (method.equals("GET")) {
+                handleGet(path, resp);
+                return;
+            }
+            if (method.equals("GET") && path.matches(API_POST_ID)) {
+                // easy way
+                final var id = Long.parseLong(path.substring(path.lastIndexOf("/")));
+                controller.getById(id, resp);
+                return;
+            }
+            if (method.equals("POST") && path.equals("/api/posts")) {
+                controller.save(req.getReader(), resp);
+                return;
+            }
+            if (method.equals("DELETE") && path.matches("/api/posts/\\d+")) {
+                // easy way
+                final var id = Long.parseLong(path.substring(path.lastIndexOf("/")));
+                controller.removeById(id, resp);
+                return;
+            }
+            resp.setStatus(HttpServletResponse.SC_NOT_FOUND);
+        } catch (Exception e) {
+            e.printStackTrace();
+            resp.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    private void handleGet(String path, HttpServletResponse resp) throws IOException {
+        if (path.equals(API_POSTS)) {
+            controller.all(resp);
+        } else if (path.equals(API_POST_ID)) {
+            final var id = Long.parseLong(path.substring(path.lastIndexOf("/")));
+            controller.getById(id, resp);
+        } else resp.setStatus(HttpServletResponse.SC_NOT_FOUND);
+    }
+}
+
